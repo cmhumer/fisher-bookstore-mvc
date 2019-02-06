fbc38e5fcbdffe79b35579670d605f55ec2f2441
Chris Humer <chrismhumer@gmail.com>
2019-02-06 18:02:23 -0500

    FeaturedAuthor

--- Fisher.Bookstore/Controllers/AuthorController.cs
+++ Fisher.Bookstore/Controllers/AuthorController.cs
@@ -1,16 +1,21 @@
 using System;
+using Fisher.Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers{
     public class AuthorController : Controller{
         public IActionResult Index(){
             //return Content("Authors");
-            return View("Author");
+            return View("Index");
         }
 
-        public IActionResult Featured(){
-            //return Content("Featured Authors");
-            return View("Author");
+        public IActionResult featured(){
+            var featuredAuthor = new Author()
+            {
+                AuthorId = 1,
+                Name = "J.K. Rowling"
+            };
+            return View(featuredAuthor);
         }
     }
 }
